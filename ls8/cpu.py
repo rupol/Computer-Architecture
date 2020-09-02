36d95c2beea88919acdafaa6e085e1f7f9bccbb3
Ruth Poliakon <r.poliakon@gmail.com>
2020-09-02 17:54:34 -0600

    inventory notes, implemented CPU constructor

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,16 @@
 
 import sys
 
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = []
+        self.reg = [0] * 8
+        self.pc = 0  # program counter, address of the currently executing instruction
+        self.running = True
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +22,32 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, MAR):
+        # return MAR (address) MDR (value)
+        pass
+
+    def ram_write(self, MDR, MAR):
+        # write MDR (value) to MAR (address)
+        pass
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +59,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +73,10 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
+        # read the memory address stored in register PC
+        # store in IR (instruction register - local variable)
+        # read bytes at pc + 1 and pc + 2 and store into operand_a and operand_b
+        # perform actions needed based on given opcode (if-elif statements)
+        # update pc to point to next instruction
+        # exit the loop if a HLT instruction is encountered (no matter what comes next)
         pass
