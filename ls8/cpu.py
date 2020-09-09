22b94c0a2b7c273d12ab9a4f08695e34189354b8
Ruth Poliakon <r.poliakon@gmail.com>
2020-09-08 19:47:26 -0600

    run loop beautified

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,6 +2,35 @@
 
 import sys
 
+# OP codes
+ADD = 0b10100000
+AND = 0b10101000
+CALL = 0b01010000
+CMP = 0b10100111
+DEC = 0b01100110
+DIV = 0b10100011
+HLT = 0b00000001
+INC = 0b01100101
+IRET = 0b00010011
+JEQ = 0b01010101
+JLE = 0b01011001
+JLT = 0b01011000
+JMP = 0b01010100
+JNE = 0b01010110
+LD = 0b10000011
+LDI = 0b10000010
+MUL = 0b10100010
+OR = 0b10101010
+POP = 0b01000110
+PRA = 0b01001000
+PRN = 0b01000111
+PUSH = 0b01000101
+RET = 0b00010001
+SHL = 0b10101100
+ST = 0b10000100
+SUB = 0b10100001
+XOR = 0b10101011
+
 
 class CPU:
     """Main CPU class."""
@@ -14,15 +43,34 @@ class CPU:
         self.reg[7] = self.sp
         self.pc = 0  # program counter, address of the currently executing instruction
         self.running = True
-        self.opcode = {
-            # set instruction codes
-            'HLT': 0b00000001,
-            'LDI': 0b10000010,
-            'PRN': 0b01000111,
-            'MUL': 0b10100010,
-            'ADD': 0b10100000,
-            'PUSH': 0b01000101,
-            'POP': 0b01000110
+        self.bt = {  # branch table
+            # ADD: self.op_add,
+            # AND: self.op_and,
+            # CALL: self.op_call,
+            # CMP: self.op_cmp,
+            # DEC: self.op_dec,
+            # DIV: self.op_div,
+            HLT: self.op_hlt,
+            # INC: self.op_inc,
+            # IRET: self.op_iret,
+            # JEQ: self.op_jeq,
+            # JLE: self.op_jle,
+            # JLT: self.op_jlt,
+            # JMP: self.op_jmp,
+            # JNE: self.op_jne,
+            # LD: self.op_ld,
+            LDI: self.op_ldi,
+            # MUL: self.op_mul,
+            # OR: self.op_or,
+            POP: self.op_pop,
+            # PRA: self.op_pra,
+            PRN: self.op_prn,
+            PUSH: self.op_push,
+            # RET: self.op_ret,
+            # SHL: self.op_shl,
+            # ST: self.op_st,
+            # SUB: self.op_sub,
+            # XOR: self.op_xor,
         }
 
     def load(self):
@@ -67,9 +115,9 @@ class CPU:
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
-        if op == self.opcode['ADD']:
+        if op == ADD:
             self.reg[reg_a] += self.reg[reg_b]
-        elif op == self.opcode['MUL']:
+        elif op == MUL:
             self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
@@ -94,6 +142,35 @@ class CPU:
 
         print()
 
+    # OP functions
+    def op_hlt(self, operand_a, operand_b):
+        # exit the loop (no matter what comes next)
+        self.running = False
+
+    def op_ldi(self, operand_a, operand_b):
+        # load "immediate", store a value in a register, or "set this register to this value"
+        # register location is byte at pc + 1 (operand_a)
+        # value is byte at pc + 2 (operand_b)
+        self.reg[operand_a] = operand_b
+
+    def op_pop(self, operand_a, operand_b):
+        # copy the value from the address pointed to by SP to the given register
+        value = self.ram_read(self.sp)
+        self.reg[operand_a] = value
+        # increment the SP
+        self.sp += 1
+
+    def op_prn(self, operand_a, operand_b):
+        # prints the numeric value stored in a register
+        # register location is byte at pc + 1 (operand_a)
+        print(self.reg[operand_a])
+
+    def op_push(self, operand_a, operand_b):
+        # decrement the SP
+        self.sp -= 1
+        # copy the value in the given register to the address pointed to by SP
+        self.ram_write(self.reg[operand_a], self.sp)
+
     def run(self):
         """Run the CPU."""
         while self.running:
@@ -108,38 +185,14 @@ class CPU:
             # determine if IR is an ALU operation
             isALU = (IR >> 5) & 0b00000001
 
-            # perform actions needed based on given opcode (if-elif statements)
-            # exit the loop if a HLT instruction is encountered (no matter what comes next)
-            if IR == self.opcode['HLT']:
-                self.running = False
-
             # dispatch ALU operations to alu function
-            elif isALU == 1:
+            if isALU == 1:
                 self.alu(IR, operand_a, operand_b)
 
-            elif IR == self.opcode['LDI']:
-                # load "immediate", store a value in a register, or "set this register to this value"
-                # register location is byte at pc + 1 (operand_a)
-                # value is byte at pc + 2 (operand_b)
-                self.reg[operand_a] = operand_b
-
-            elif IR == self.opcode['PRN']:
-                # prints the numeric value stored in a register
-                # register location is byte at pc + 1 (operand_a)
-                print(self.reg[operand_a])
-
-            elif IR == self.opcode['PUSH']:
-                # decrement the SP
-                self.sp -= 1
-                # copy the value in the given register to the address pointed to by SP
-                self.ram_write(self.reg[operand_a], self.sp)
-
-            elif IR == self.opcode['POP']:
-                # copy the value from the address pointed to by SP to the given register
-                value = self.ram_read(self.sp)
-                self.reg[operand_a] = value
-                # increment the SP
-                self.sp += 1
+            # perform actions needed based on given opcode (branch table)
+            else:
+                if IR in self.bt:
+                    self.bt[IR](operand_a, operand_b)
 
             # determine number of operands
             num_operands = IR >> 6
